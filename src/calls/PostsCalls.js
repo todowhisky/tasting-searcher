e070d114469143ab9afc90073cd9af83d8daa53b
todowhisky <info@todowhisky.es>
2019-12-03 14:38:31 +0100

    Create new call

--- src/calls/PostsCalls.js
+++ src/calls/PostsCalls.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 const PostCalls = {
   getPosts: async (params) => {
-    const response = await axios.get('https://todowhisky.es/wp-json/wp/v2/posts', {
+    const response = await axios.get('https://todowhisky.es/wp-json/wp/v2/posts?_embed=1', {
+      params: {...params }
+    })
+    return response;
+  },
+  getCategories: async (params) => {
+    const response = await axios.get('https://todowhisky.es/wp-json/wp/v2/categories?parent=3123', {
       params: {...params }
     })
     return response;
